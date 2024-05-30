b4d24982eb5a621a8f90d973dc39b84352955d8f
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2024-05-30 08:55:48 -0500

    feat(project): :sparkles: @HttpCode(200/404/500) -> Htttp status error

--- src/hello/hello.controller.ts
+++ src/hello/hello.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Req, Res } from '@nestjs/common';
+import { Controller, Get, HttpCode, Req, Res } from '@nestjs/common';
 import { Request, Response } from 'express';
 
 @Controller('')
@@ -6,5 +6,17 @@ export class HelloController {
   @Get('/')
   getHello(@Req() request: Request, @Res() response: Response) {
     response.status(200).json({ message: 'Hello World!' });
+  }
+
+  @Get('/notfound')
+  @HttpCode(404)
+  notFoundPage() {
+    return { message: 'Page not found' };
+  }
+
+  @Get('/error')
+  @HttpCode(500)
+  errorPage() {
+    return { message: 'Error route!!!' };
   }
 }

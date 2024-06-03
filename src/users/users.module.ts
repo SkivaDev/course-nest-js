66d1dcc6f95f5a8bc7ec3be208812ae31f633573
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2024-06-03 12:37:18 -0500

    feat(project): :sparkles: Database

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,21 +3,22 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { LoggerMiddleware } from './logger/logger.middleware';
 import { AuthMiddleware } from './auth/auth.middleware';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService, PrismaService]
 })
 export class UsersModule implements NestModule{
   configure(consumer: import("@nestjs/common").MiddlewareConsumer) {
-    consumer
-    .apply(LoggerMiddleware)
-    // .forRoutes('users'); // Aplica el middleware a todas "/users" las rutas de la aplicación
-    .forRoutes( // Aplica el middleware a las rutas especificadas "/users"
-      {path: '/users', method: RequestMethod.GET},
-      {path: '/users', method: RequestMethod.POST}
-    )
-    .apply(AuthMiddleware)
-    .forRoutes('/users');
+    // consumer
+    // .apply(LoggerMiddleware)
+    // // .forRoutes('users'); // Aplica el middleware a todas "/users" las rutas de la aplicación
+    // .forRoutes( // Aplica el middleware a las rutas especificadas "/users"
+    //   {path: '/users', method: RequestMethod.GET},
+    //   {path: '/users', method: RequestMethod.POST}
+    // )
+    // .apply(AuthMiddleware) // Aplica el middleware a todas las rutas de la aplicación
+    // .forRoutes('/users');
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,25 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class UsersService {
-  private users = [
-    {
-      id: 1,
-      name: 'John Doe',
-      email: 'jhon@gmial.com',
-    },
-    {
-      id: 2,
-      name: 'Jane Doe',
-      email: 'jane@gmail.com',
-    },
-  ];
-  getUsers(): any[] {
-    return this.users;
+
+  constructor(private prisma: PrismaService) {}
+
+  getUsers(): any {
+    return this.prisma.user.findMany();
   }
 
   createUser(user: any): any {
-    this.users.push(user);
-    return user;
+    return this.prisma.user.create({ data: user});
   }
 }
